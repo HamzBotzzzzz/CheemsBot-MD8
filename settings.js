84ac6f1d4c19617ef2a720bc00b92e4aa8a01ab1
Hamzzzzz <109900268+HamzBotzzzzz@users.noreply.github.com>
2024-03-12 20:42:15 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,36 +21,36 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.owner = ['6288214989352'] //ur owner number
+global.ownernomer = "6288888888" //ur owner number2
+global.ownername = "IlhamGantenkkk" //ur owner name
+global.ytname = "YT: Keong" //ur yt chanel name
+global.socialm = "GitHub: Susu" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
+global.botname = "Harukana - MD v1"
+global.ownernumber = '6288214989352'
+global.ownername = 'IlhamGantenkk'
+global.ownerNumber = ["6288214989352@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/xnxn"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.wagc = "https://chat.whatsapp.com/FMgp91EgeQC7CIOAf9VZJl"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.wm = "Harukana - MD"
+global.botscript = 'https://github.com/Haci' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "Ilham Gantenkk\n\nHarukana - MD v1"
+global.creator = "6288214989352@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
 
 //media target
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync("./XeonMedia/theme/Haruka.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/Haruka.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/Haruka.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/Haruka.jpg") //ur thumb pic
 global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
 
 //menu image maker
